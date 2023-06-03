5977ecacda6084e89847592d7c295efabe481e19
Omkar Limkar <70531387+omkarlimkar@users.noreply.github.com>
2023-06-03 10:07:18 +0530

    Add files via upload

---
+++ WAp to perform basic operation using python function.py
@@ -0,0 +1,24 @@
+def add(x, y):
+    """Function to add two numbers"""
+    return x + y
+
+def subtract(x, y):
+    """Function to subtract two numbers"""
+    return x - y
+
+def multiply(x, y):
+    """Function to multiply two numbers"""
+    return x * y
+
+def divide(x, y):
+    """Function to divide two numbers"""
+    return x / y
+
+# Main program
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print("Addition:", add(num1, num2))
+print("Subtraction:", subtract(num1, num2))
+print("Multiplication:", multiply(num1, num2))
+print("Division:", divide(num1, num2))
